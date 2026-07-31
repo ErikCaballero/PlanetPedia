9e932d03ae46f2a78242860bcce245c76c94cf5e
Erik <e.rodriguezmoreno@ikasle.eus>
2026-07-31 13:21:55 +0200

    Finalización de los Garbilos e introducción de los Wikilinks
    He introducido mediante css y un pelin de javascript una opción para que al pasar el ratón por encima de un link aparezca algo de contenido de la especie del link.

---
+++ Web/razas/wikilink.js
@@ -0,0 +1,7 @@
+tippy('.especie-link', {
+        allowHTML: true,       // Permite meter código HTML (imágenes y párrafos) dentro del popup
+        theme: 'wiki',         // Usa el diseño personalizado que creamos arriba
+        animation: 'scale',    // Efecto de aparición suave
+        placement: 'top',      // Posición por defecto (arriba del enlace)
+        interactive: true      // Permite al usuario mover el ratón dentro del popup si lo necesita
+    });
